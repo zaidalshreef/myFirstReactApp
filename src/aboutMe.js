3c7959172ef62c1a267d3643da8e73c8158a7c7b
zaidalsharif <zaidalshreef@gmail.com>
2022-06-16 16:21:52 +0300

    refactor

---
+++ src/aboutMe.js
@@ -0,0 +1,11 @@
+
+
+export default function Description(){
+    return (
+    <div className="description-container">
+    <h1>About Me</h1>
+      <p>I am a frontend developer with a passion for creating beautiful and functional websites. I have a background in business and a passion for learning new technologies. I am currently working as a frontend developer at.</p>
+   <h1>Interests</h1>
+  <p>I am currently working on a project called .</p>          
+  </div>)
+}
---
+++ src/contact.js
@@ -0,0 +1,11 @@
+
+
+
+export default function ContactComponent(){
+    return (
+    <div className="contact-container">
+    <button className="email-button" ></button>
+    <button className="linkedin-button"></button>
+  </div>
+  )
+}
